adea836f60857da92f7e1ba1ba96785cbb9084de
Furqan Athar <furqanatharpak@gmail.com>
2025-07-17 13:12:58 +0500

    Add comment to serializer explaining its purpose

--- shortener/serializers.py
+++ shortener/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import ShortURL
+# Serializer to convert ShortURL model instances to and from JSON
 class ShortURLSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShortURL
         fields = '__all__'
-        # These fields are auto-managed by the backend and should not be modified by the user
-        read_only_fields = ['id', 'short_code', 'created_at', 'updated_at', 'access_count']
+
         
